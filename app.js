2ef833ca869818a00e053c0dcf5abbfd5e1dbb2e
jose torres <jtorres.pdx@gmail.com>
2015-09-27 19:36:33 -0700

    route to homepage added.

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 var studentlogin = angular.module('studentlogin', ['ui.router']);
+
+  studentlogin.config(function($stateProvider, $urlRouterProvider) {
+    $stateProvider.state('home', {
+      url: "",
+      templateUrl: "partials/home.html"
+    });
+  });
