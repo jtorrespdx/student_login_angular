c300e8679f44f9951aa3002cd6a3393fee75502f
jose torres <jtorres.pdx@gmail.com>
2015-09-27 21:41:29 -0700

    hard coded students

--- services/StudentsFactory.js
+++ services/StudentsFactory.js
@@ -1,6 +1,15 @@
 signIn.factory('StudentsFactory', function StudentsFactory() {
   var factory = {};
 
-  factory.students = [];
+  factory.students = [
+    {name: 'Jose Torres', signedIn: false, signedOut: false},
+    {name: 'Eli Gonzalez', signedIn: false, signedOut: false},
+    {name: 'Jessica Fix', signedIn: false, signedOut: false},
+    {name: 'Sonci Carey', signedIn: false, signedOut: false},
+    {name: 'Fred Stooges', signedIn: false, signedOut: false},
+    {name: 'Jason Franklin', signedIn: false, signedOut: false},
+    {name: 'Hannah Jersey', signedIn: false, signedOut: false},
+    {name: 'Big Mary', signedIn: false, signedOut: false},
+  ];
   return factory;
 });
